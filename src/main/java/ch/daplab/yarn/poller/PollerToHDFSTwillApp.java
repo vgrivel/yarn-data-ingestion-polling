1254fe70b93a953aabb55499474448450ff835f0
Vincent Grivel <vincent.grivel@x-apps.com>
2015-05-05 14:08:17 +0200

    correct small bugs

--- src/main/java/ch/daplab/yarn/poller/PollerToHDFSTwillApp.java
+++ src/main/java/ch/daplab/yarn/poller/PollerToHDFSTwillApp.java
@@ -21,7 +21,7 @@ import static ch.daplab.yarn.poller.PollerToHDFSCli.*;
 public class PollerToHDFSTwillApp extends AbstractTwillRunnable {
 
     private static final Logger LOG = LoggerFactory.getLogger(PollerToHDFSTwillApp.class);
-
+    private final OptionParser parser = new OptionParser();
     /**
      * Called by YARN nodeManager, i.e. remote (not on the same JVM) from the command line
      * which startsd it.
@@ -29,21 +29,22 @@ public class PollerToHDFSTwillApp extends AbstractTwillRunnable {
     @Override
     public void run() {
 
-        final OptionParser parser = new OptionParser();
+        privateInitParser();
         parser.allowsUnrecognizedOptions();
         final OptionSet optionSet = parser.parse(getContext().getApplicationArguments());
 
         final Configuration conf = new Configuration();
 
         String defaultFs = (String) optionSet.valueOf(OPTION_FS_DEFAULTFS);
-
+        LOG.info("defaultFs: "+defaultFs);
         String url = (String) optionSet.valueOf(OPTION_URL);
-        boolean etagSupport = (boolean) optionSet.valueOf(OPTION_ETAG_SUPPORT);
-        long intervalms = (long) optionSet.valueOf(OPTION_INTERVAL_MS);
+        LOG.info("OPTION_URL: "+defaultFs);
         String rootFolder = (String) optionSet.valueOf(OPTION_ROOT_FOLDER);
         String fileSuffix = (String) optionSet.valueOf(OPTION_FILE_SUFFIX);
         String partitionFormat = (String) optionSet.valueOf(OPTION_PARTITION_FORMAT);
         String parsingClass = (String) optionSet.valueOf(OPTION_PARSING_CLASS);
+        boolean etagSupport = Boolean.valueOf((String)optionSet.valueOf(OPTION_ETAG_SUPPORT));
+        long intervalMs = Long.parseLong((String)optionSet.valueOf(OPTION_INTERVAL_MS));
 
         if (defaultFs != null) {
             conf.set("fs.defaultFS", defaultFs);
@@ -54,7 +55,7 @@ public class PollerToHDFSTwillApp extends AbstractTwillRunnable {
             fs = FileSystem.get(FileSystem.getDefaultUri(conf), conf);
 
             LOG.info("PartitionFormat "+ partitionFormat);
-            Observable.create(new PollerObservable(url, etagSupport, intervalms, parsingClass)).subscribe(new PartitionedObserver(rootFolder, partitionFormat, fileSuffix, fs));
+            Observable.create(new PollerObservable(url, etagSupport, intervalMs, parsingClass)).subscribe(new PartitionedObserver(rootFolder, partitionFormat, fileSuffix, fs));
 
         } catch (IOException e) {
             LOG.error("Got an IOException", e);
@@ -68,6 +69,18 @@ public class PollerToHDFSTwillApp extends AbstractTwillRunnable {
               //  }
             }
         }
+    }
+
+    private void privateInitParser() {
+        parser.accepts(OPTION_FS_DEFAULTFS);
+        parser.accepts(OPTION_URL);
+        parser.accepts(OPTION_ETAG_SUPPORT);
+        parser.accepts(OPTION_INTERVAL_MS);
+        parser.accepts(OPTION_ROOT_FOLDER);
+        parser.accepts(OPTION_FILE_SUFFIX);
+        parser.accepts(OPTION_PARTITION_FORMAT);
+        parser.accepts(OPTION_PARSING_CLASS);
+
     }
 }
 
